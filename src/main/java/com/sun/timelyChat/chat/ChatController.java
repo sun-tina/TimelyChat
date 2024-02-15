54e3645eda6bc47a71f2dc913021720768530e58
Tina Sun <suntina@google.com>
2024-02-15 14:20:25 -0500

    add controller class

---
+++ src/main/java/com/sun/timelyChat/chat/ChatController.java
@@ -0,0 +1,11 @@
+package com.sun.timelyChat.chat;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class ChatController {
+    //add user
+
+    // send message
+}
--- src/main/java/com/sun/timelyChat/config/WebSocketEventListener.java
+++ src/main/java/com/sun/timelyChat/config/WebSocketEventListener.java
@@ -1,5 +1,6 @@
 package com.sun.timelyChat.config;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebSocketEventListener {
 
+    @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
         //implement later
     }
