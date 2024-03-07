690eec1fdd4af7bba1cb43010e54210f27a6103c
Tina Sun <suntina@google.com>
2024-03-07 01:59:31 -0500

    create new sql database for user

---
+++ src/main/database/userDatabase.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE chat;
+
+USE chat;
+
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(64) NOT NULL,
+  email VARCHAR(64) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY username_UNIQUE (email)
+);
